46c67dcfd1e5234068969e22e8acf1e1bdc83263
Rebecca Congi <rncongi@gmail.com>
2020-10-27 14:47:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       item_name = req.path.split("/items/").last 
       if @@items.include?("item_name")
       
-        item = @@items.find{|i| i.name == item_name
+        item = @@items.find{|i| i.name == item_name}
       
       resp.write item.price
       
