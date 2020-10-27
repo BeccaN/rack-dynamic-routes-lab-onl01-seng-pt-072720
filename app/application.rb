0612ff5e408391a3b8e19758a155165bc2d51e0e
Rebecca Congi <rncongi@gmail.com>
2020-10-27 14:38:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,12 @@
 class Application
-  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      
+    end
+    resp.finish
+  end
+
 end 
