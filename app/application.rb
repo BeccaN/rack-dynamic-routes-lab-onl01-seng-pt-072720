2c35afc20b98a42aa4d2bb8e223aad5288216374
Rebecca Congi <rncongi@gmail.com>
2020-10-27 14:47:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Application
     if req.path.match(/items/)
  
       item_name = req.path.split("/items/").last 
-      if @@items.include? 
+      if @@items.include?("item_name")
       
-      item = @@items.find{|i| i.name == item_name}
+        item = @@items.find{|i| i.name == item_name
       
       resp.write item.price
       
